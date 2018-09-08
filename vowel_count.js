078e2da848d37050f1a854a9846b4857f93ece6b
Tayte Stokes <taytestokes9328@gmail.com>
2018-09-07 21:38:45 -0600

    Added a new toy problem vowel_count

---
+++ vowel_count.js
@@ -0,0 +1,43 @@
+//Step 1: Clarify
+    //Create a function that returns a count of how many vowels are in a string passed in as an argument
+    //We will consider a,e,i,o,u as vowels
+
+//Step 2: Create sample data and solve without code
+    //'Tayte Stokes' ==> 4
+    //'I love code' ==> 5
+    //'Apple MacBook' ==> 5
+
+//Step 3: Define code constructs
+    //Function
+    //for loop
+        //forEach()
+    //If statement
+    // ===
+    // ||
+
+//Step 4: Psuedo Code
+    //Create a function that accepts a string as an argument and will return a count of how many vowels are in the string
+    //Define a counter variable and set it equal to 0
+    //Convert the string into lowercase and store it in a variable
+    //Split the lowercase string and store it into a new variable
+    //Loop through the split lowercase array
+        //For each element in the array, check if they are a vowel (a,e,i,o,u)
+        //If they are, increase the counter variable by 1
+    //Return the counter variable
+
+//Step 5: Solve with code
+    function vowelCount(str){
+        let counter = 0;
+        let lowerCaseString = str.toLowerCase();
+        let lowerCaseArray = lowerCaseString.split('');
+        for(let i = 0; i < lowerCaseArray.length; i++){
+            if(lowerCaseArray[i] == 'a' || lowerCaseArray[i] == 'e' || lowerCaseArray[i] == 'i' || lowerCaseArray[i] == 'o' || lowerCaseArray[i] == 'u'){
+                counter += 1;
+            }
+        }
+        console.log(counter);
+    }
+
+//Step 6: test
+let stringOne = 'Tayte Stokes';
+vowelCount(stringOne);
